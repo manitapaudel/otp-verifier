3bdf19af418461b6b929ffd850a098a34d23b0a6
Manita Paudel <paudel04manita@gmail.com>
2024-06-04 21:21:57 +0545

    Add border and placeholder

--- src/app/components/number-input/index.tsx
+++ src/app/components/number-input/index.tsx
@@ -9,10 +9,13 @@ const NumberInput = ({ name }: NumberInput) => {
 
   return (
     <input
-      className="text-9xl text-center text-textColor bg-secondary max-w-44 h-auto max-h-52 py-4 rounded-lg cursor-pointer"
+      className={`text-9xl text-center text-textColor bg-secondary max-w-44 h-auto max-h-52 py-4 rounded-lg cursor-pointer placeholder:opacity-30 ${
+        value ? "border border-1 border-textColor" : ""
+      }`}
       type="number"
       name={name}
       value={value}
+      placeholder="0"
     />
   );
 };
