7bc45ee7078cc51cb38439d2003edf95ec174370
Manita Paudel <paudel04manita@gmail.com>
2024-06-05 17:38:01 +0545

    Add event l;istener to the input fields

--- src/app/components/number-input/index.tsx
+++ src/app/components/number-input/index.tsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { forwardRef, KeyboardEventHandler, useState } from "react";
 
-type NumberInput = {
+type NumberInputProps = {
   name: string;
+  handleKeyUp: KeyboardEventHandler<HTMLInputElement>;
 };
 
-const NumberInput = ({ name }: NumberInput) => {
-  const [value, setValue] = useState<number>(null);
+const NumberInput = forwardRef<HTMLInputElement, NumberInputProps>(
+  ({ name, handleKeyUp }, ref) => {
+    const [value] = useState<number | null>(null);
 
-  return (
-    <input
-      className={`text-9xl text-center text-textColor bg-secondary max-w-44 h-auto max-h-52 py-4 rounded-lg cursor-pointer placeholder:opacity-30 ${
-        value ? "border border-1 border-textColor" : ""
-      }`}
-      type="number"
-      name={name}
-      value={value}
-      placeholder="0"
-    />
-  );
-};
+    return (
+      <input
+        className={`text-9xl text-center text-textColor bg-secondary max-w-44 h-auto max-h-52 py-4 rounded-lg cursor-pointer placeholder:opacity-30 ${
+          value ? "border border-1 border-textColor" : ""
+        }`}
+        type="number"
+        name={name}
+        placeholder="0"
+        onKeyDown={handleKeyUp}
+        ref={ref}
+        maxLength={1}
+      />
+    );
+  }
+);
+
+// Setting the display name for the component
+NumberInput.displayName = "NumberInput";
 
 export default NumberInput;
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,55 @@
 "use client";
 
+import { useRef } from "react";
 import Button from "./components/button";
 import NumberInput from "./components/number-input";
 
 export default function Home() {
+  const inputRefs = [useRef(), useRef(), useRef(), useRef()];
+
+  const handleKeyUp = (e, index) => {
+    const inputElement = inputRefs[index].current;
+
+    const currentLength = inputElement.value.length;
+    const maxLength = inputElement.maxLength;
+
+    if (e.key === "Backspace") {
+      if (index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
+    } else {
+      const nextElement = inputRefs[index + 1]?.current;
+      if (currentLength === maxLength) {
+        nextElement.focus();
+      }
+    }
+  };
+
   return (
     <main className="flex text-textColor text-center font-merriweather min-h-screen flex-col items-center justify-between mt-20">
       <div className="bg-primary w-2/3 h-auto p-14 rounded-3xl">
         <h1 className="text-3xl">Verify your email address</h1>
         <p className="text-sm w-2/3 mx-auto mt-3 mb-12">
-          A four-digit code has been sent to your email
-          name@frontendpro.dev.Please enter the code below to verify your email
-          address.
+          A four-digit code has been sent to your email name@frontendpro.dev.
+          Please enter the code below to verify your email address.
         </p>
         <div className="grid grid-cols-4 gap-5 mb-16">
-          <NumberInput />
-          <NumberInput />
-          <NumberInput />
-          <NumberInput />
+          <NumberInput
+            ref={inputRefs[0]}
+            handleKeyUp={(e) => handleKeyUp(e, 0)}
+          />
+          <NumberInput
+            ref={inputRefs[1]}
+            handleKeyUp={(e) => handleKeyUp(e, 1)}
+          />
+          <NumberInput
+            ref={inputRefs[2]}
+            handleKeyUp={(e) => handleKeyUp(e, 2)}
+          />
+          <NumberInput
+            ref={inputRefs[3]}
+            handleKeyUp={(e) => handleKeyUp(e, 3)}
+          />
         </div>
         <Button label="Verify OTP" />
       </div>
